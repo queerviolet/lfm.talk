a8f926ffe29072f780462aab37e48c3a90c2cb5d
Ashi Krishnan <queerviolet@github.com>
2018-07-27 22:25:59 -0400

    Fixed fetch script.

--- fetch.sh
+++ fetch.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-curl -O http://lfm.ashi.io/assets/{\
-cerebellum-cross-section.mp4,\
-deep-dream-layers.mp4,gan-faces.mp4,\
-miquel_pn_deep_dream.mp4,\
+
+curl --create-dirs http://lfm.ashi.io/\{\
 5ht2a.m4v,\
+adversarial-skiiers.gif,\
 adversarial-sticker.m4v,\
-brain-skull-to-vision.mp4,\
-retinal-structure.m4v} -o assets/#1
+brain-skull-to-vision.m4v,\
+gan-faces.mp4,\
+gray-cerebellum-pink.png,\
+gray-cerebellum-side-invert.png,\
+gray-cerebellum-side.png,\
+lsd-binding-lid.png,\
+miquel_pn_deep_dream.mp4,\
+retinal-structure.m4v,\
+sobo-cerebellum-back-invert.png,\
+sobo-cerebellum-back.png,\
+sobo-cerebellum-pink.png,\
+\} -o assets/#1
+
+# gsutil cp gs://lfm.ashi.io/{\
+# gan-faces.mp4,\
+# miquel_pn_deep_dream.mp4,\
+# 5ht2a.m4v,\
+# adversarial-sticker.m4v,\
+# brain-skull-to-vision.mp4,\
+# retinal-structure.m4v} gs://lfm.ashi.io/assets/
