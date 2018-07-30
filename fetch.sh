805623cb6c3094d64469b9d0ca783a4ca38e99ad
Ashi Krishnan <queerviolet@github.com>
2018-07-29 23:30:51 -0400

    Update fetch list.

--- fetch.sh
+++ fetch.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-
 curl --create-dirs http://lfm.ashi.io/\{\
 5ht2a.m4v,\
 adversarial-skiiers.gif,\
 adversarial-sticker.m4v,\
+box-illusion-cube.png,\
 brain-skull-to-vision.m4v,\
 gan-faces.mp4,\
 gray-cerebellum-pink.png,\
 gray-cerebellum-side-invert.png,\
 gray-cerebellum-side.png,\
 lsd-binding-lid.png,\
+man-laugh.wav,\
 miquel_pn_deep_dream.mp4,\
+munker-confetti.jpg_large,\
 retinal-structure.m4v,\
+sobbing.wav,\
 sobo-cerebellum-back-invert.png,\
 sobo-cerebellum-back.png,\
 sobo-cerebellum-pink.png,\
+vis-heirarchy-macaque.gif,\
+visual-cortex.jpg,\
+woman-laugh.wav\
 \} -o assets/#1
-
-# gsutil cp gs://lfm.ashi.io/{\
-# gan-faces.mp4,\
-# miquel_pn_deep_dream.mp4,\
-# 5ht2a.m4v,\
-# adversarial-sticker.m4v,\
-# brain-skull-to-vision.mp4,\
-# retinal-structure.m4v} gs://lfm.ashi.io/assets/
