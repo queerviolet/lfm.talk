eb58bee52290ce70dc25e8903eae7a626e6203b9
Ashi Krishnan <queerviolet@github.com>
2019-10-04 09:51:44 +0200

    Updates for Fronteers Amsterdam 2019

--- fetch.sh
+++ fetch.sh
@@ -7,9 +7,12 @@ adversarial-sticker.m4v,\
 box-illusion-cube.png,\
 brain-skull-to-vision.m4v,\
 gan-faces.mp4,\
+gan-problems-counting-3.png,\
+gan-problems-fallout-cow.png,\
 gray-cerebellum-pink.png,\
 gray-cerebellum-side-invert.png,\
 gray-cerebellum-side.png,\
+laughcry.mp3,\
 lsd-binding-lid.png,\
 man-laugh.wav,\
 miquel_pn_deep_dream.mp4,\
@@ -19,7 +22,8 @@ sobbing.wav,\
 sobo-cerebellum-back-invert.png,\
 sobo-cerebellum-back.png,\
 sobo-cerebellum-pink.png,\
+triangle-grid.png,\
 vis-heirarchy-macaque.gif,\
-visual-cortex.jpg,\
-woman-laugh.wav\
+visual-cortex.png,\
+woman-laugh.wav,\
 \} -o assets/#1
